7875e39482122ba6bf1d9cc6562c1e9bca63d08b
Michael Weinberger <michael.weinberger@pangea.cloud>
2023-08-18 12:30:31 -0400

    adding additional audit fields

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const core = require('@actions/core');
-const pangea = require('pangea-node-sdk')
+const github = require('@actions/github');
+const pangea = require('pangea-node-sdk');
 
 const token = core.getInput('token');
 const config = new pangea.PangeaConfig({ domain: core.getInput('domain')});
 const audit = new pangea.AuditService(token, config);
+const context = github.context;
 
 // most @actions toolkit packages have async methods
 async function run() {
   const data = {
+    action: context.github.action,
+    actor: context.github.actor,
     message: core.getInput('text'),
+    source: "GitHub Action",
   };
 
-  try {
+  try{
     core.info("Logging: %s", data.message);
     const logResponse = await audit.log(data, {verbose: true});
     core.info("Response: %s", logResponse.result);
